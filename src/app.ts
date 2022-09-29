1b59b7da7cbc757e5daa21d733d7e6a7a54213d9
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-09-29 10:57:49 +0200

    can POST to /api/data

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     return res.send("TS wins")
+})
+
+app.post("/api/data", (req, res) => {
+    console.log(req.body)
+    return res.send(200, "you got it")
 })
 
 app.listen(port, () =>{
