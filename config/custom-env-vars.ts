227fc0b771ac5dcfb488abb5511ab1a42e3b42eb
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-09-30 18:18:11 +0200

    forming the project

---
+++ config/custom-env-vars.ts
@@ -0,0 +1,7 @@
+export default {
+    accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
+    accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
+    refreshTokenPrivateKey: "REFRESH_PRIVATE_KEY",
+    dbUrl: "DB_CONNECTION",
+    port: "PORT"
+}
